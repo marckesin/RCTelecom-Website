777175f471f5e9a184c6fdb19ee7e459366edd95
marckesin <192.168.2.1@protonmail.com>
2022-06-23 15:06:10 -0300

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,13 @@
 // Animate on Screen
+// eslint-disable-next-line no-undef
 AOS.init();
-
-// Toggle navbar
-window.onscroll = () => scrollFunction();
 
 const scrollFunction = () => {
   const toTopIcon = document.getElementById("to-top");
   const whatsappIcon = document.getElementById("whatsapp");
+  const { scrollTop } = document.body;
 
-  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+  if (scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
     document.getElementById("navbar").style.top = "-100px";
     toTopIcon.classList.add("slide-left");
     whatsappIcon.classList.add("slide-left");
@@ -17,19 +16,7 @@ const scrollFunction = () => {
     toTopIcon.classList.remove("slide-left");
     whatsappIcon.classList.remove("slide-left");
   }
-}
+};
 
-
-// const pos = { x: 0, y: 0 };
-
-// const saveCursorPosition = function (x, y) {
-//   pos.x = (x / window.innerWidth).toFixed(2);
-//   pos.y = (y / window.innerHeight).toFixed(2);
-//   document.documentElement.style.setProperty('--x', pos.x);
-//   document.documentElement.style.setProperty('--y', pos.y);
-// }
-
-// document.addEventListener('mousemove', e => {
-//   saveCursorPosition(e.clientX, e.clientY);
-//   console.log(e.clientX, e.clientY);
-// })
+// Toggle navbar
+window.onscroll = () => scrollFunction();
